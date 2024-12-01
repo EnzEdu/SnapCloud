e5291eb57a88ef5bd2c7aab8c25dd532edacdf74
Kathelleen <kathelleenv@gmail.com>
2024-12-01 00:04:40 -0300

    Design de telas #05

---
+++ static/js/login.js
@@ -0,0 +1,9 @@
+// Não leve o usuário para páginas anteriores.
+window.addEventListener("load", function() {
+    if (window.history && window.history.pushState) {
+        window.history.pushState(null, null, window.location.href);
+        window.onpopstate = function () {
+            window.history.pushState(null, null, window.location.href);
+        };
+    }
+});
