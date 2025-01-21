cf81e178375ba2a39e640618cd83f3a597ce5776
EnzEdu <enzinhovlogs@outlook.com>
2025-01-21 01:49:11 -0300

    feat: Edição de perfil com RDS

--- edit_profile.py
+++ edit_profile.py
@@ -3,8 +3,8 @@ from werkzeug.utils import secure_filename
 from werkzeug.security import generate_password_hash, check_password_hash
 from utilities import allowed_file
 import os
-import sqlite3
-from utilities import DB_NAME
+from rds import RDS_DATABASE, Usuario
+import pymysql
 
 bp = Blueprint("edit_profile", __name__)
 
@@ -21,34 +21,46 @@ def edit_profile():
             description = request.form["descricao"]
             # profile_picture = request.form[] # Foto de perfil foi removida do prototipo 1
 
-            conn = sqlite3.connect(DB_NAME, timeout = 10)
-            cursor = conn.cursor()
-
-            is_the_password_the_same = password == session["hashed_password"]
+            is_the_password_the_same = (password == session["hashed_password"])
 
             try:
                 # senha menor do que 6 caracteres ou inalterada
                 if len(password) < 6 or is_the_password_the_same:
-                    sql = "UPDATE users SET full_name = ?, username = ?, email = ?, description = ? WHERE id = ?"
-                    cursor.execute(sql, (full_name, username, email, description, session.get('id')))
+                    
+                    result = RDS_DATABASE.session.query(Usuario).filter_by(username=username)
+                    if result is not None:
+                        raise pymysql.err.IntegrityError("")
+                    
+                    RDS_DATABASE.session.query(Usuario).filter_by(id=session.get("id")).update({
+                        "full_name" : full_name,
+                        "username": username,
+                        "email": email,
+                        "description": description
+                    })
+
+                    RDS_DATABASE.session.commit()
 
                     if len(password) < 6:
                         flash("Não são permitidas senhas com menos de 6 caracteres", "erro")
                         return redirect(url_for('dashboard.dashboard'))
                 
                 else:
-                    sql = "UPDATE users SET full_name = ?, username = ?, email = ?, password = ?, description = ? WHERE id = ?"
-                    password = generate_password_hash(password) # hasheia a nova senha
-                    cursor.execute(sql, (full_name, username, email, password, description, session.get('id')))
-                
-                # cursor.execute("UPDATE users SET full_name = ?, username = ?, email = ?, password = ?, description = ? WHERE id = ?", (full_name, username, email, password, description, session.get('id')))
-                conn.commit()
-                cursor.close()
-                conn.close()
+                    hashed_password = generate_password_hash(password) # hasheia a nova senha
+
+                    RDS_DATABASE.session.query(Usuario).filter_by(id=session.get("id")).update({
+                        "full_name" : full_name,
+                        "username": username,
+                        "email": email,
+                        "password": hashed_password,
+                        "description": description
+                    })
+                    RDS_DATABASE.session.commit()
+
                 session["hashed_password"] = password # salva a nova senha ou senha inalterada na sessão
 
-            except sqlite3.IntegrityError as e:
-                flash("Nome de usuario já em uso", "erro") # testando se usar o mesmo nome de usuario vai cair aqui
+            except Exception as ex:
+                if isinstance(ex, pymysql.err.IntegrityError):
+                    flash("Nome de usuario já em uso", "erro") # testando se usar o mesmo nome de usuario vai cair aqui
                 return redirect(url_for('dashboard.dashboard'))
 
             flash("Dados válidos atualizados com sucesso", "sucesso")
@@ -56,17 +68,15 @@ def edit_profile():
 
         if request.method == 'GET':
             # Recupera as informações para mostrar na página de edição
-            conn = sqlite3.connect(DB_NAME, timeout=10)
-            conn.row_factory = sqlite3.Row
-            cursor = conn.cursor()
-            
-            user = cursor.execute("select * from users where id = ?", (session.get('id'), )).fetchone()
-            session["hashed_password"] = user["password"] # Solução improvisada para verificar se a senha mudou. Isso é passado para o edit_profile.html
-            cursor.close()
-            conn.close()
+            user = RDS_DATABASE.session.query(Usuario).filter_by(id=session.get('id')).first()
 
-            return render_template('edit_profile.html', user = user)
-    
+            if user is not None:
+                session["hashed_password"] = user.password
+                return render_template("edit_profile.html", user=user)
+            else:
+                flash("Você não está logado!", "erro")
+                return redirect(url_for('auth.login'))
+
     else:
         flash("Você não está logado!", "erro")
         return redirect(url_for('auth.login'))
