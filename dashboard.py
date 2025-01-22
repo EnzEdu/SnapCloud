b813c81b99a3a442556c061d43db38bfdd3a4d6e
EnzEdu <enzinhovlogs@outlook.com>
2025-01-22 12:15:21 -0300

    feat: Pesquisa utilizando palavras-chave

--- dashboard.py
+++ dashboard.py
@@ -13,7 +13,27 @@ def dashboard():
             audios = RDS_DATABASE.session.query(Audio).filter_by(id_usuario=session.get("id")).all()
             imagens = RDS_DATABASE.session.query(Imagem).filter_by(id_usuario=session.get("id")).all()
 
-        return render_template("dashboard.html", user=usuario, audios=audios, imagens=imagens)
+            return render_template("dashboard.html", user=usuario, audios=audios, imagens=imagens)
+        else:
+            filter = request.form["filter"]
+            query = request.form["query"]
+
+            audios_encontrados = ""
+            imagens_encontradas = ""
+
+            if filter == "nome":
+                audios_encontrados = RDS_DATABASE.session.query(Audio).filter_by(id_usuario=usuario.id, file_name=query).all()
+                imagens_encontradas = RDS_DATABASE.session.query(Imagem).filter_by(id_usuario=usuario.id, file_name=query).all()
+
+            elif filter == "descricao":
+                audios_encontrados = RDS_DATABASE.session.query(Audio).filter(Audio.description.contains(query)).all()
+                imagens_encontradas = RDS_DATABASE.session.query(Imagem).filter(Imagem.description.contains(query)).all()
+
+            else:
+                audios_encontrados = RDS_DATABASE.session.query(Audio).filter(Audio.tags.contains(query)).all()
+                imagens_encontradas = RDS_DATABASE.session.query(Imagem).filter(Imagem.tags.contains(query)).all()
+
+            return render_template("dashboard.html", user=usuario, audios=audios_encontrados, imagens=imagens_encontradas)
 
     else:
         flash("Você não está logado!", "erro")
